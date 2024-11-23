3cbbec62485dbd26870fc4154e358c3ecffbdc91
jelohe <jsus.90@gmail.com>
2024-11-23 01:46:21 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import Card from 'react-bootstrap/Card';
 import './App.css';
+
+function CodeCard({ name, code }) {
+  return (
+    <Card className="m-3 bg-secondary text-white">
+      <Card.Header>{name}</Card.Header>
+      <Card.Body>
+        <Card.Title>{code}</Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function AddCodeCard() {
+  return (
+    <Card className="bg-primary m-3 text-white">
+      <Card.Body>
+        <Card.Title>+</Card.Title>
+      </Card.Body>
+    </Card>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="fixed-top">
+        <CodeCard name="Github" code="321654" />
+        <CodeCard name="Google" code="987645" />
+        <AddCodeCard />
+      </div>
+      <div className="App-header bg-dark">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
