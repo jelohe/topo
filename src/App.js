2753700929e46a904215bac1d42987e8ac17213f
jelohe <jsus.90@gmail.com>
2024-11-24 01:48:55 +0100

    Refresh tokens every 30 sec

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useLocalStorage } from '@uidotdev/usehooks';
-import logo from './logo.svg';
-import AddCodeModal from './components/AddCodeModal';
-import CodeCard from './components/CodeCard';
-import './App.css';
+import { useLocalStorage } from '@uidotdev/usehooks'
+import logo from './logo.svg'
+import AddCodeModal from './components/AddCodeModal'
+import CodeCard from './components/CodeCard'
+import './App.css'
+
+function SecretsToCards({ secrets }) {
+  return secrets.length
+      ? secrets.flatMap(secretToCard)
+      : null
+}
+
+function secretToCard(secret) {
+  return Object.entries(secret).map(([n, s]) => (
+    <CodeCard name={n} secret={s} key={n}/>
+  ))
+}
 
 function App() {
-  const [secrets] = useLocalStorage("secrets", "");
-  const secretCards = secrets.length
-      ? secrets.flatMap(obj => (
-        Object.entries(obj).map(([n, s]) => <CodeCard name={n} secret={s} key={n}/>)
-      ))
-      : null;
+  const [secrets] = useLocalStorage("secrets", "")
 
   const appStyle = {
     overflowX: 'hidden',
@@ -23,7 +30,7 @@ function App() {
     <>
     <div className="App">
       <div className="fixed-top" style={appStyle}>
-        {secretCards}
+        <SecretsToCards secrets={secrets} />
         <AddCodeModal />
       </div>
       <div className="App-header" style={{ backgroundColor: "#bcbcbc" }}>
@@ -31,7 +38,7 @@ function App() {
       </div>
     </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/CodeCard.js
+++ src/components/CodeCard.js
@@ -1,31 +1,56 @@
+import { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import { TOTP } from 'totp-generator';
 import Theme from '../constants/theme';
 
-function CodeCard({ name, secret }) {
-  const { otp: rawCode } = TOTP.generate(secret, { encoding: "ascii" })
-  const prettyCode = rawCode.slice(0,3) + " " + rawCode.slice(3);
-
-  const cardStyle = {
-    backgroundColor: Theme.gray15,
-    color: Theme.black,
-    cursor: 'alias'
-  };
+function CodeCardHeader({ name }) {
   const headerStyle = {
     fontWeight: 'thin',
     fontStyle: 'italic',
     letterSpacing: '2px',
     fontSize: Theme.md
   };
+  return <Card.Header style={headerStyle}>{name}</Card.Header>
+}
+
+function CodeCardBody({ code }) {
   const codeStyle = { letterSpacing: Theme.sm, fontSize: Theme.lg };
-  const toClipboard = _e => navigator.clipboard.writeText(rawCode);
+  return (
+    <Card.Body>
+      <Card.Title style={codeStyle}>{code}</Card.Title>
+    </Card.Body>
+  )
+}
+
+function rawToPretty(code) {
+  return `${code.slice(0, 3)} ${code.slice(3)}`
+}
+
+function generateRawCode(secret) {
+  const config = { encoding: 'ascii' }
+  const { otp } = TOTP.generate(secret, config)
+  return otp
+}
+
+function CodeCard({ name, secret }) {
+  const cardStyle = {
+    backgroundColor: Theme.gray15,
+    color: Theme.black,
+    cursor: 'alias'
+  };
+
+  const pollingTime = 30000
+  const [code, setCode] = useState(generateRawCode(secret));
+  useEffect(() => { 
+    setInterval(() => setCode(generateRawCode(secret)), pollingTime)
+  }, [secret])
+
+  const toClipboard = _ => navigator.clipboard.writeText(code);
 
   return (
     <Card className="CodeCard m-3" style={cardStyle} onClick={toClipboard}>
-      <Card.Header style={headerStyle}>{name}</Card.Header>
-      <Card.Body>
-        <Card.Title style={codeStyle}>{prettyCode}</Card.Title>
-      </Card.Body>
+      <CodeCardHeader name={name} />
+      <CodeCardBody code={rawToPretty(code)} />
     </Card>
   );
 }
