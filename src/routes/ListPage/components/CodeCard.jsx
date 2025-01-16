ecdd88d0a4f402f35dbd6db3ec9731a97cfe9983
jelohe <jsus.90@gmail.com>
2025-01-16 21:17:43 +0100

    Extract code generation logic to the code-list page

--- src/routes/ListPage/components/CodeCard.jsx
+++ src/routes/ListPage/components/CodeCard.jsx
@@ -1,32 +1,18 @@
 import { useState, useEffect } from 'react';
 import { TOTP } from 'totp-generator';
 
-function CodeCard({ name, secret }) {
-  if (!name || !secret) return null;
+function CodeCard({ name, code }) {
+  if (!name || !code) return null;
 
-  const [code, setCode] = useState(generateRawCode(secret));
-
-  useEffect(() => { 
-    const pollingTime = 30000
-    const intervalId = setInterval(() => setCode(generateRawCode(secret)), pollingTime)
-    return () => clearInterval(intervalId)
-  })
-
-  function generateRawCode(secret) {
-    const config = { encoding: 'ascii' }
-    const { otp } = TOTP.generate(secret, config)
-    return otp
-  }
-  
   const toClip = () => navigator.clipboard.writeText(code)
   const pretty = `${code.slice(0, 3)} ${code.slice(3)}`
 
   return (
-    <article class="message">
-      <div class="message-header is-size-5">
+    <article className="message">
+      <div className="message-header is-size-5">
         <p>{name}</p>
       </div>
-      <button onClick={toClip} class="button is-fullwidth message-body">
+      <button onClick={toClip} className="button is-fullwidth message-body">
         <p className="is-size-2">{pretty}</p>
       </button>
     </article>
--- src/routes/ListPage/index.jsx
+++ src/routes/ListPage/index.jsx
@@ -1,37 +1,56 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router'
 import { useLocalStorage } from '@uidotdev/usehooks'
+import { TOTP } from 'totp-generator';
 import CodeCard from './components/CodeCard'
+
+const TIMEOUT_S = 30
+const TIMEOUT_MS = TIMEOUT_S * 100
+
+function generateCodes(secrets) {
+  function generateRawCode(secret) {
+    const config = { encoding: 'ascii', period: TIMEOUT_S }
+    const { otp } = TOTP.generate(secret, config)
+    return otp
+  }
+
+  const rawCodes = Object
+    .entries(secrets)
+    .map(([k, v]) => [k, generateRawCode(v)])
+
+  return Object.fromEntries(rawCodes)
+}
 
 function ListPage() {
   const [secrets] = useLocalStorage("secrets", "")
+  const [codes, setCodes] = useState(generateCodes(secrets));
   const navigate = useNavigate()
 
-  const style = {
-    overflowX: 'hidden',
-    overflowY: 'auto',
-    top: 0,
-    bottom: 0
-  }
+  useEffect(() => { 
+    const codesInterval = setInterval(() => 
+      setCodes(generateCodes(secrets)), TIMEOUT_MS)
+    return () => clearInterval(codesInterval)
+  }, [secrets])
 
   return (
     <>
-      <h1 class="title">Topo Auth</h1>
+      <h1 className="title">Topo Auth</h1>
       <hr />
-      <div class="content has-text-centered">
-        <CodeList secrets={secrets} />
+      <div className="content has-text-centered">
+        <CodeList codes={codes} />
       </div>
       <button
         onClick={() => navigate('/add')}
-        class="button is-primary is-large is-fullwidth">+</button>
+        className="button is-primary is-large is-fullwidth">+</button>
     </>
   )
 }
 
-function CodeList({ secrets }) {
+function CodeList({ codes }) {
   return Object
-    .keys(secrets)
+    .keys(codes)
     .map(name => (
-      <CodeCard name={name} key={name} secret={secrets[name]} />
+      <CodeCard name={name} key={name} code={codes[name]} />
     ))
 }
 
