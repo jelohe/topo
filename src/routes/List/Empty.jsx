e0835ffc2b5387f38fdf20f369edc6c7feb30b65
Txus <jsus.90@gmail.com>
2025-02-13 07:19:45 +0100

    Update Empty.jsx

--- src/routes/List/Empty.jsx
+++ src/routes/List/Empty.jsx
@@ -1,15 +1,8 @@
 import { useNavigate } from 'react-router'
 import { useState, useEffect } from 'react';
-import TextScramble from '@/lib/textScramble';
 
 export default function Empty() {
   const navigate = useNavigate()
-
-  useEffect(() => {
-    const el = document.querySelector('.neo');
-    const fx = new TextScramble(el);
-    fx.setText("326 629");
-  });
 
   return (
     <>
@@ -19,10 +12,10 @@ export default function Empty() {
       <hr/>
       <div className="content is-medium">
         <p>
-          Topo allows you to store <span className="has-text-primary">Secrets</span> and generate temporary access <span className="has-text-primary">Codes</span>.
+          Topo allows you to store <span className="has-text-primary">Secrets</span> and generate temporary access <span className="has-text-primary">Codes</span> for two factor authentication.
         </p>
         <p>
-          Secrets are usually provided as <span className="has-text-primary">QR Codes</span>. The safest backup for this secrets is to store a paper print of the QR or an image file in a safe digital storage.
+          Secrets are usually provided as <span className="has-text-primary">QR Codes</span>. You should keep the QR image somewhere safe as a backup.
         </p>
         <p>Start by scanning a QR code to store your first secret.</p>
       </div>
@@ -31,8 +24,6 @@ export default function Empty() {
           className="button is-primary is-fullwidth">
         <img src="/images/qr.svg" alt="qr-icon" width="35" height="35" />
       </button>
-      <p className="neo">
-      </p>
     </>
   );
 }
