acfd64102dd54b264468d302f4ea49c749faa7dd
jelohe <jsus.90@gmail.com>
2025-02-13 07:02:34 +0100

    Give better feedback when copying a code

--- src/routes/List/CodeCard.jsx
+++ src/routes/List/CodeCard.jsx
@@ -4,10 +4,14 @@ import TextScramble from '@/lib/textScramble';
 
 function CodeCard({ name, code }) {
   if (!name || !code) return null;
-  const toClip = () => navigator.clipboard.writeText(code)
-  const pretty = `${code.slice(0, 3)} ${code.slice(3)}`
-
   const el = useRef(null);
+
+  const pretty = `${code.slice(0, 3)} ${code.slice(3)}`
+  const toClip = () => {
+    const fx = new TextScramble(el.current);
+    fx.setText("Copied!");
+    setTimeout(() => fx.setText(pretty), 3000);
+  }
 
   useEffect(() => {
     const fx = new TextScramble(el.current);
@@ -15,14 +19,16 @@ function CodeCard({ name, code }) {
   }, [pretty])
 
   return (
-    <article className="message">
-      <div className="message-header is-size-5">
-        <p>{name}</p>
-      </div>
-      <button onClick={toClip} className="button is-fullwidth message-body">
-        <p ref={el} className="is-size-2">{pretty}</p>
-      </button>
-    </article>
+    <>
+      <article className="is-fullwidth message is-clickable">
+        <div className="message-header is-size-5">
+          <p>{name}</p>
+        </div>
+        <div onClick={toClip} className="message-body">
+          <p ref={el} className="is-size-2">{pretty}</p>
+        </div>
+      </article>
+    </>
   );
 }
 
